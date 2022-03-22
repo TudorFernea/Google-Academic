fe69b3e3dcd712fe99121df0d85ed1cf5c7c35d1
TudorFernea <37572351+TudorFernea@users.noreply.github.com>
2022-03-22 09:20:01 +0200

    Add files via upload
    first hello world

---
+++ App/src/main/java/com/example/google_academic/GoogleAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.example.google_academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAcademicApplication.class, args);
+	}
+
+}
---
+++ App/src/test/java/com/example/google_academic/GoogleAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.google_academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
